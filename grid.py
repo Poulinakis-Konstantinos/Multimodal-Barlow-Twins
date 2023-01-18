da9b9672c0308ea093a0bb5302583ca7bf6b21ec
Konstantinos Poulinakis <poulinakis.kon@gmail.com>
2023-01-18 14:13:21 +0200

    Time id for runs

--- grid.py
+++ grid.py
@@ -5,9 +5,11 @@ from slp.config.config_parser import make_cli_parser, parse_config
 from slp.plbind.dm import PLDataModuleFromDatasets
 import ruamel.yaml
 from sklearn.model_selection import ParameterGrid 
+from datetime import datetime
+
 
 if __name__=='__main__':
-    config_path = '/home/poulinakis/Multimodal-Barlow-Twins/configs/my-config.yml'
+    config_path = '/home/poulinakis/Multimodal-Barlow-Twins/configs/my-config_3.yml'
     # load the config
     yaml = ruamel.yaml.YAML()
     # yaml.preserve_quotes = True
@@ -15,21 +17,23 @@ if __name__=='__main__':
         data = yaml.load(fp)
 
     # Define a search space for the parameters
-    params = {'hidden_size': [50, 70], # model
-              'num_layers': [1, 4],
-              'bidirectional': [False, True],
-              'dropout': [0.2, 0.5],
-              'weight_decay_ssl': [0.0, 0.3],   # optimization
-              'weight_decay': [0.0, 0.5],
-              'lr_ssl': [5e-2, 5e-4],
+    params = {'hidden_size': [100], # model
+              'num_layers': [1],
+              'bidirectional': [False],
+              'dropout': [0.2],
+              'weight_decay_ssl': [0.0],   # optimization
+              'weight_decay': [0.0],
+              'lr_ssl': [5e-4],
               'lr': [5e-3],
-
-              'freeze_grads': [True]
+              'batch_size': [16, 32, 64] , #[128, 156, 182],
+              'freeze_grads': [False],
+              'gauss_noise_p': [[0.7, 0.2], [0.9, 0.1]] #,[[0.0, 0.0], [0.5, 0.5]] #,  
             }
 
+    data['trainer']['experiment_name'] = 'Test=batch_Pnoise'
     for i, parameters in enumerate(list(ParameterGrid(params))):
-        data['trainer']['experiment_name'] = 'Gauss_Noise-Grid_Model_params'
         fr = parameters['freeze_grads']
+        data['run_name'] = f'freeze_grads={fr}_{str(datetime.now())}'
 
         # change the configuration file's values
         data['model']['hidden_size'] = parameters['hidden_size']
@@ -41,9 +45,15 @@ if __name__=='__main__':
         data['ssl_optimization']['optim']['lr'] = parameters['lr_ssl']
         data['optimization']['optim']['weight_decay'] = parameters['weight_decay']
         data['optimization']['optim']['lr'] = parameters['lr']
-
-        data['run_name'] = f'grid_model_{i}_freeze_grads={fr}'
+        
         data['tune']['freeze_grads'] = parameters['freeze_grads']
+
+        # batch size
+        data['data']['batch_size'] = parameters['batch_size']
+        data['data']['batch_size_eval'] = parameters['batch_size']
+
+        # SSL transformations
+        data['transformations']['gauss_noise_p'] = parameters['gauss_noise_p']
 
         # save new config values 
         with open(config_path, 'w') as fp:
@@ -55,6 +65,6 @@ if __name__=='__main__':
         config = parse_config(parser, config_path)
 
         # Call training script
-        command = f'python3 experiments/mm.py --config configs/my-config.yml --gpus 1 '
+        command = f'python3 experiments/mm.py --config {config_path} --gpus 1 '
         os.system(command)
     #subprocess.call(["python", "experiments/mm.py", "--config configs/my-config.yml", "--offline"])
